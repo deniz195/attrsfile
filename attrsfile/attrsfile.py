e6c39a22c71bef704573aa9571665722658091d6
Deniz Bozyigit <dbozyigit@battrion.com>
2021-01-28 17:07:12 +0100

    fixing doc build pipeline

--- attrsfile/attrsfile.py
+++ attrsfile/attrsfile.py
@@ -8,24 +8,24 @@ from ruamel.yaml import YAML
 module_logger = logging.getLogger(__name__)
 module_logger.setLevel(logging.INFO)
 
-class MapperFactory:
 
+class MapperFactory:
     def __init__(self, path_format):
         self.path_format = path_format
-    
+
     def patch(self, cls):
         self.cls = cls
-        cls.mapper_factory =  self
-                
+        cls.mapper_factory = self
+
         self.cls.save = lambda obj: self.save(obj)
         self.cls.load = lambda **data: self.load(cls, **data)
         self.cls.filename = lambda obj: self.filename(obj)
         return self.cls
-    
+
     def save(self, obj):
         data = self.data(obj)
         filename = self._filename(data)
-        
+
         module_logger.debug(f'save {obj} to {filename} (from {self.path_format})')
 
         with open(filename, 'w') as s:
@@ -34,19 +34,18 @@ class MapperFactory:
             yaml.dump(data, s)
 
         return filename
-        
 
     def load(self, cls, **data):
         filename = self._filename(data)
-        
+
         module_logger.debug(f'load {cls} from {filename} (from {self.path_format})')
 
         with open(filename, 'r') as s:
-            yaml=YAML()
+            yaml = YAML()
             data_loaded = yaml.load(s)
-            
+
         return self.build(cls, data_loaded)
-            
+
     def build(self, cls, data):
         if hasattr(cls, 'from_dict'):
             return cls.from_dict(data)
@@ -54,7 +53,7 @@ class MapperFactory:
             return cls(**data)
         else:
             return cls(**data)
-        
+
     def data(self, obj):
         if hasattr(obj, 'to_dict'):
             return obj.to_dict()
@@ -62,40 +61,39 @@ class MapperFactory:
             return attr.asdict(obj)
         else:
             return dict(obj)
-        
+
     def _filename(self, data):
         return self.path_format.format(**data)
-        
+
     def filename(self, obj):
         data = self.data(obj)
         return self._filename(data)
-        
-        
+
+
 def map_file(path_format):
     def wrapper(cls):
         m = MapperFactory(path_format)
         cls = m.patch(cls)
-        
+
         return cls
-        
+
     return wrapper
+
 
 mapper = map_file
 attrsfile = map_file
-        
-        
-# @map_file('abc{x}.yaml')        
+
+
+# @map_file('abc{x}.yaml')
 # @attr.s()
 # class D:
 
 #     x = attr.ib(1)
 #     y = attr.ib(2)
-    
+
 #     def to_dict(self):
 #         return attr.asdict(self)
-        
+
 #     @classmethod
 #     def from_dict(cls, d):
 #         return cls(**d)
-        
-    
--- tests/test_attrsfile.py
+++ tests/test_attrsfile.py
@@ -8,14 +8,12 @@ import attr
 from attrsfile import attrsfile
 
 
-
-
 @pytest.fixture
 def response():
-    '''Sample pytest fixture.
+    """Sample pytest fixture.
 
     See more at: http://doc.pytest.org/en/latest/fixture.html
-    '''
+    """
     # import requests
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
@@ -26,23 +24,20 @@ def test_content(response):
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
 
 
-
-
 def test_d():
-    @attrsfile('abc{x}.yaml')        
+    @attrsfile('abc{x}.yaml')
     @attr.s()
     class D:
 
         x = attr.ib(1)
         y = attr.ib(2)
-        
+
         def to_dict(self):
             return attr.asdict(self)
-            
+
         @classmethod
         def from_dict(cls, d):
             return cls(**d)
-                
 
     d1 = D(x=99, y=42)
     d1.to_dict()
@@ -50,21 +45,21 @@ def test_d():
     assert d1.save() == 'abc99.yaml'
 
     import pytest
+
     with pytest.raises(FileNotFoundError):
         D.load(x=2)
-    
+
     d2 = D.load(x=99)
     assert d1 == d2
 
-    
 
 def test_attr():
-    @attrsfile('xyz.yaml')        
+    @attrsfile('xyz.yaml')
     @attr.s()
     class XYZ:
         x = attr.ib(1)
-        y = attr.ib(2) 
-        
+        y = attr.ib(2)
+
     x1 = XYZ()
     assert x1.filename() == 'xyz.yaml'
 
@@ -76,4 +71,3 @@ def test_attr():
 def test_import():
     from attrsfile import attrsfile
     from attrsfile import mapper
-    
